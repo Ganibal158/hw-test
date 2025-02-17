1b350b8f2af773ae6ab12a8af0f79a97d94a856b
Kirill <kuznetsokirill@yandex.ru>
2025-02-17 22:21:26 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	// Place your code here.
+	fmt.Println(String("Hello, OTUS!"))
 }
+
+func String(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+
+// пакет "golang.org/x/example/hello/reverse" не хотел подключаться "could not import golang.org/x/example/hello/reverse (no required module provides package "golang.org/x/example/hello/reverse")compilerBrokenImport" И подчёркивался красным, пришлось засунуть функцию сюда. Подскажите, пожалуйста, что можно было сделать?
